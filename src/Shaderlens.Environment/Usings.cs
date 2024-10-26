7f0c2e1bf2cd53da6cd59fc71e240b4945516de3
ytt <ytt0x@pm.me>
2024-10-26 15:15:46 -0700

    fix: Environment - Add ivec and uvec functions usings

--- src/Shaderlens.Environment/Usings.cs
+++ src/Shaderlens.Environment/Usings.cs
@@ -6,6 +6,12 @@ global using static Shaderlens.Environment<Shaderlens.Float>;
 global using static Shaderlens.Environment<Shaderlens.vec2>;
 global using static Shaderlens.Environment<Shaderlens.vec3>;
 global using static Shaderlens.Environment<Shaderlens.vec4>;
+global using static Shaderlens.Environment<Shaderlens.ivec2>;
+global using static Shaderlens.Environment<Shaderlens.ivec3>;
+global using static Shaderlens.Environment<Shaderlens.ivec4>;
+global using static Shaderlens.Environment<Shaderlens.uvec2>;
+global using static Shaderlens.Environment<Shaderlens.uvec3>;
+global using static Shaderlens.Environment<Shaderlens.uvec4>;
 
 global using _float = Shaderlens.Float;
 global using float_ = Shaderlens.Float;
