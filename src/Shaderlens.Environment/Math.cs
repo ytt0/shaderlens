fb17d72e7efc47e915c7c67ea70122c2136f99a5
ytt <ytt0x@pm.me>
2024-10-26 15:17:26 -0700

    improve: Environment - Add pack functions

--- src/Shaderlens.Environment/Math.cs
+++ src/Shaderlens.Environment/Math.cs
@@ -34,6 +34,87 @@
 
         public static int length<T>(this T[] array) { return default; }
 
+
+        #region 8.3 Common Functions
+
+        // Returns a signed integer value representing the encoding of a Float. The Float value's bit-level representation is preserved.
+        public static int floatBitsToInt(Float value) { return default; }
+        // Returns an unsigned integer value representing the encoding of a Float. The Float value's bit-level representation is preserved.
+        public static uint floatBitsToUint(Float value) { return default; }
+        // Returns a float value corresponding to a signed integer encoding of a Float. If a NaN is passed in, it will not signal, and the resulting value is unspecified. If an Inf is passed in, the resulting value is the corresponding Inf.
+        public static int intBitsToFloat(Float value) { return default; }
+        // Returns a float value corresponding to an unsigned integer encoding of a Float. If a NaN is passed in, it will not signal, and the resulting value is unspecified. If an Inf is passed in, the resulting value is the corresponding Inf.
+        public static uint uintBitsToFloat(Float value) { return default; }
+
+        #endregion
+
+
+        #region 8.4 Floating-Point Pack and Unpack Functions
+
+        // First, converts each component of the normalized floating-point value v into 16-bit (2x16) or 8-bit (4x8) integer values. Then, the results are packed into the returned 32-bit unsigned integer.
+        // The conversion for component c of v to fixed point is done as follows:
+        // packUnorm2x16: round(clamp(c, 0, +1) * 65535.0)
+        // The first component of the vector will be written to the least significant bits of the output; the last component will be written to the most significant bits.
+        public static uint packUnorm2x16(vec2 v) { return default; }
+
+        // First, converts each component of the normalized floating-point value v into 16-bit (2x16) or 8-bit (4x8) integer values. Then, the results are packed into the returned 32-bit unsigned integer.
+        // The conversion for component c of v to fixed point is done as follows:
+        // packSnorm2x16: round(clamp(c, -1, +1) * 32767.0)
+        // The first component of the vector will be written to the least significant bits of the output; the last component will be written to the most significant bits.
+        public static uint packSnorm2x16(vec2 v) { return default; }
+
+        // First, converts each component of the normalized floating-point value v into 16-bit (2x16) or 8-bit (4x8) integer values. Then, the results are packed into the returned 32-bit unsigned integer.
+        // The conversion for component c of v to fixed point is done as follows:
+        // packUnorm4x8: round(clamp(c, 0, +1) * 255.0)
+        // The first component of the vector will be written to the least significant bits of the output; the last component will be written to the most significant bits.
+        public static uint packUnorm4x8(vec4 v) { return default; }
+
+        // First, converts each component of the normalized floating-point value v into 16-bit (2x16) or 8-bit (4x8) integer values. Then, the results are packed into the returned 32-bit unsigned integer.
+        // The conversion for component c of v to fixed point is done as follows:
+        // packSnorm4x8: round(clamp(c, -1, +1) * 127.0)
+        // The first component of the vector will be written to the least significant bits of the output; the last component will be written to the most significant bits.
+        public static uint packSnorm4x8(vec4 v) { return default; }
+
+        // First, unpacks a single 32-bit unsigned integer p into a pair of 16-bit unsigned integers, a pair of 16-bit signed integers, four 8-bit unsigned integers, or four 8-bit signed integers, respectively. Then, each component is converted to a normalized floating-point value to generate the returned two- or four-component vector.
+        // The conversion for unpacked fixed-point value f to floating-point is done as follows:
+        // unpackUnorm2x16: f / 65535.0
+        // The first component of the returned vector will be extracted from the least significant bits of the input; the last component will be extracted from the most significant bits.
+        public static vec2 unpackUnorm2x16(uint p) { return default; }
+
+        // First, unpacks a single 32-bit unsigned integer p into a pair of 16-bit unsigned integers, a pair of 16-bit signed integers, four 8-bit unsigned integers, or four 8-bit signed integers, respectively. Then, each component is converted to a normalized floating-point value to generate the returned two- or four-component vector.
+        // The conversion for unpacked fixed-point value f to floating-point is done as follows:
+        // unpackSnorm2x16: clamp(f / 32767.0, -1, +1)
+        // The first component of the returned vector will be extracted from the least significant bits of the input; the last component will be extracted from the most significant bits.
+        public static vec2 unpackSnorm2x16(uint p) { return default; }
+
+        // First, unpacks a single 32-bit unsigned integer p into a pair of 16-bit unsigned integers, a pair of 16-bit signed integers, four 8-bit unsigned integers, or four 8-bit signed integers, respectively. Then, each component is converted to a normalized floating-point value to generate the returned two- or four-component vector.
+        // The conversion for unpacked fixed-point value f to floating-point is done as follows:
+        // unpackUnorm4x8: f / 255.0
+        // The first component of the returned vector will be extracted from the least significant bits of the input; the last component will be extracted from the most significant bits.
+        public static vec4 unpackUnorm4x8(uint p) { return default; }
+
+        // First, unpacks a single 32-bit unsigned integer p into a pair of 16-bit unsigned integers, a pair of 16-bit signed integers, four 8-bit unsigned integers, or four 8-bit signed integers, respectively. Then, each component is converted to a normalized floating-point value to generate the returned two- or four-component vector.
+        // The conversion for unpacked fixed-point value f to floating-point is done as follows:
+        // unpackSnorm4x8: clamp(f / 127.0, -1, +1)
+        // The first component of the returned vector will be extracted from the least significant bits of the input; the last component will be extracted from the most significant bits.
+        public static vec4 unpackSnorm4x8(uint p) { return default; }
+
+        // Returns an unsigned integer obtained by converting the components of a two-component floating-point vector to the 16-bit floating-point representation of the API, and then packing these two 16-bit integers into a 32-bit unsigned integer.
+        // The first vector component specifies the 16 least-significant bits of the result; the second component specifies the 16 most-significant bits.
+        public static uint packHalf2x16(vec2 v) { return default; }
+
+        // Returns a two-component floating-point vector with components obtained by unpacking a 32-bit unsigned integer into a pair of 16-bit values, interpreting those values as 16-bit floating-point numbers according to the API, and converting them to 32-bit floating-point values.
+        // The first component of the vector is obtained from the 16 least-significant bits of v; the second component is obtained from the 16 most- significant bits of v.
+        public static vec2 unpackHalf2x16(uint v) { return default; }
+
+        // Returns a double-precision value obtained by packing the components of v into a 64-bit value. If an IEEE 754 Inf or NaN is created, it will not signal, and the resulting floating-point value is unspecified. Otherwise, the bit-level representation of v is preserved. The first vector component specifies the 32 least significant bits; the second component specifies the 32 most significant bits.
+        public static double packDouble2x32(uvec2 v) { return default; }
+
+        // Returns a two-component unsigned integer vector representation of v. The bit-level representation of v is preserved. The first component of the vector contains the 32 least significant bits of the double; the second component consists of the 32 most significant bits.
+        public static uvec2 unpackDouble2x32(double v) { return default; }
+
+        #endregion
+
         #region 8.5 Geometric Functions
         public static vec3 cross(vec3 x, vec3 y) { return default; }
         // Available only when using the compatibility profile. For core OpenGL, use invariant.
@@ -210,45 +291,45 @@
         public static genType ceil(genType x) { return default; }
         // Returns x – floor (x)
         public static genType fract(genType x) { return default; }
-        // -
+        // Modulus. Returns x – y ∗ floor (x/y).
         public static genType mod(genType x, Float y) { return default; }
         // Modulus. Returns x – y ∗ floor (x/y).
         public static genType mod(genType x, genType y) { return default; }
-        // Returns the fractional part of x and sets i to the integer part (as a whole number doubleing-point value). Both the return value and the output parameter will have the same sign as x.
+        // Returns the fractional part of x and sets i to the integer part (as a whole number floating-point value). Both the return value and the output parameter will have the same sign as x.
         public static genType modf(genType x, out genType i) { i = default; return default; }
-        // -
+        // Returns y if y < x; otherwise it returns x.
         public static genType min(genType x, genType y) { return default; }
-        // -
+        // Returns y if y < x; otherwise it returns x.
         public static genType min(genType x, Float y) { return default; }
-        // -
+        // Returns y if y < x; otherwise it returns x.
         public static genType min(genType x, int y) { return default; }
         // Returns y if y < x; otherwise it returns x.
         public static genType min(genType x, uint y) { return default; }
-        // -
+        // Returns y if x < y; otherwise it returns x.
         public static genType max(genType x, genType y) { return default; }
-        // -
+        // Returns y if x < y; otherwise it returns x.
         public static genType max(genType x, Float y) { return default; }
-        // -
+        // Returns y if x < y; otherwise it returns x.
         public static genType max(genType x, int y) { return default; }
         // Returns y if x < y; otherwise it returns x.
         public static genType max(genType x, uint y) { return default; }
-        // -
+        // Returns min (max (x, minVal), maxVal). Results are undefined if minVal > maxVal.
         public static genType clamp(genType x, genType minVal, genType maxVal) { return default; }
-        // -
+        // Returns min (max (x, minVal), maxVal). Results are undefined if minVal > maxVal.
         public static genType clamp(genType x, Float minVal, Float maxVal) { return default; }
-        // -
+        // Returns min (max (x, minVal), maxVal). Results are undefined if minVal > maxVal.
         public static genType clamp(genType x, int minVal, int maxVal) { return default; }
-        //  Returns min (max (x, minVal), maxVal). Results are undefined if minVal > maxVal.
+        // Returns min (max (x, minVal), maxVal). Results are undefined if minVal > maxVal.
         public static genType clamp(genType x, uint minVal, uint maxVal) { return default; }
         // Returns the linear blend of x and y, i.e., x⋅(1−a ) + y⋅a
         public static genType mix(genType x, genType y, Float a) { return default; }
-        // Selects which vector each returned component comes from. For a component of a that is false, the corresponding component of x is returned. For a component of a that is true, the corresponding component of y is returned. Components of x and y that are not selected are allowed to be invalid doubleing-point values and will have no effect on the results. Thus, this provides different functionality than, for example,
+        // Selects which vector each returned component comes from. For a component of a that is false, the corresponding component of x is returned. For a component of a that is true, the corresponding component of y is returned. Components of x and y that are not selected are allowed to be invalid floating-point values and will have no effect on the results. Thus, this provides different functionality than, for example,
         public static genType mix(genType x, genType y, genType a) { return default; }
-        // -
+        // Returns 0.0 if x < edge; otherwise it returns 1.0.
         public static genType step(genType edge, genType x) { return default; }
         // Returns 0.0 if x < edge; otherwise it returns 1.0.
         public static genType step(Float edge, genType x) { return default; }
-        // -
+        // Returns 0.0 if x <= edge0 and 1.0 if x >= edge1 and performs smooth Hermite interpolation between 0 and 1 when edge0 < x < edge1. This is useful in cases where you would want a threshold function with a smooth transition. This is equivalent to: genType t; t = clamp ((x – edge0) / (edge1 – edge0), 0, 1); return t * t * (3 – 2 * t); (And similarly for doubles.) Results are undefined if edge0 >= edge1.
         public static genType smoothstep(genType edge0, genType edge1, genType x) { return default; }
         // Returns 0.0 if x <= edge0 and 1.0 if x >= edge1 and performs smooth Hermite interpolation between 0 and 1 when edge0 < x < edge1. This is useful in cases where you would want a threshold function with a smooth transition. This is equivalent to: genType t; t = clamp ((x – edge0) / (edge1 – edge0), 0, 1); return t * t * (3 – 2 * t); (And similarly for doubles.) Results are undefined if edge0 >= edge1.
         public static genType smoothstep(Float edge0, Float edge1, genType x) { return default; }
@@ -256,17 +337,9 @@
         public static genType isnan(genType x) { return default; }
         // Returns true if x holds a positive infinity or negative infinity. Returns false otherwise.
         public static genType isinf(genType x) { return default; }
-        // -
-        public static genType doubleBitsToInt(genType value) { return default; }
-        // Returns a signed or unsigned integer value representing the encoding of a Float. The Float value's bit-level representation is preserved.
-        public static genType doubleBitsToUint(genType value) { return default; }
-        // -
-        public static genType intBitsTodouble(genType value) { return default; }
-        // Returns a Float value corresponding to a signed or unsigned integer encoding of a Float. If a NaN is passed in, it will not signal, and the resulting value is unspecified. If an Inf is passed in, the resulting value is the corresponding Inf.
-        public static genType uintBitsTodouble(genType value) { return default; }
         // Computes and returns a*b + c. In uses where the return value is eventually consumed by a variable declared as precise: • fma() is considered a single operation, whereas the expression “a*b + c” consumed by a variable declared precise is considered two operations. • The precision of fma() can differ from the precision of the expression “a*b + c”. • fma() will be computed with the same precision as any other fma() consumed by a precise variable, giving invariant results for the same input values of a, b, and c. Otherwise, in the absence of precise consumption, there are no special constraints on the number of operations or difference in precision between fma() and the expression “a*b + c”.
         public static genType fma(genType a, genType b, genType c) { return default; }
-        // Splits x into a doubleing-point significand in the range [0.5, 1.0) and an integral exponent of two, such that: x= significand⋅2exponent The significand is returned by the function and the exponent is returned in the parameter exp. For a doubleing-point value of zero, the significand and exponent are both zero. For a doubleing-point value that is an infinity or is not a number, the results are undefined. If an implementation supports negative 0, frexp(-0) should return -0; otherwise it will return 0.
+        // Splits x into a floating-point significand in the range [0.5, 1.0) and an integral exponent of two, such that: x= significand⋅2exponent The significand is returned by the function and the exponent is returned in the parameter exp. For a floating-point value of zero, the significand and exponent are both zero. For a floating-point value that is an infinity or is not a number, the results are undefined. If an implementation supports negative 0, frexp(-0) should return -0; otherwise it will return 0.
         public static genType frexp(genType x, out genType exp) { exp = default; return default; }
         // Builds a floating-point number from x and the corresponding integral exponent of two in exp, returning: significand⋅2exponent If this product is too large to be represented in the floating-point type, the result is undefined.
         public static genType ldexp(genType x, in genType exp) { return default; }
