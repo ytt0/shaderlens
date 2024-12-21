763336bf9f73bac9f1079ff8980afa4f4b0625cd
ytt <ytt0x@pm.me>
2024-12-20 20:12:30 -0800

    resource: Templates - Compute shader

---
+++ resources/Templates/Compute/ImagePassName.glsl
@@ -0,0 +1,16 @@
+﻿void main()
+{
+    ivec2 texelCoord = ivec2(gl_GlobalInvocationID.xy);
+
+    vec2 localUv = vec2(gl_LocalInvocationID.xy) / vec2(gl_WorkGroupSize.xy);
+    vec2 globalUv = vec2(gl_GlobalInvocationID.xy) / vec2(gl_NumWorkGroups.xy * gl_WorkGroupSize.xy);
+
+    vec2 uv = localUv;
+
+    // Time varying pixel color
+    float hue = 0.5 * sin(0.2 * (iTime + uv.x)) + 0.5 * sin(0.2 * (iTime + uv.y) + 1.0);
+    vec3 col = vec3(sin(hue * 6.28), sin((hue + 0.33) * 6.28), sin((hue + 0.66) * 6.28)) * 0.3 + 0.7;
+
+    // Output to screen
+    imageStore(OutputTexture0, texelCoord, vec4(col, 1.0));
+}
